cfd6f32c37f50fd8c22601cdfcd9f5670ef553b0
Alvaro <agarest1@alumno.upv.es>
2025-10-05 18:34:35 +0200

    Terminado

---
+++ DyV.h
@@ -0,0 +1,23 @@
+#ifndef DYV_H
+#define DYV_H
+
+#include <vector>
+using namespace std;
+
+template <typename T>
+
+int BusquedaBinaria (const vector <T>& v, T x, int ini, int fin){
+	
+	if(ini > fin)
+		return -1;
+	int medio = (ini + fin) / 2;
+
+	if (v[medio] == x)
+		return medio;
+	else if (v[medio] > x)
+		return BusquedaBinaria(v, x, ini, medio -1)
+	else 
+		return BusquedaBinaria(v, x, medio + 1, fin);
+}
+
+#endif
