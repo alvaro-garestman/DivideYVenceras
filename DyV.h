4dcaaad337a5f77381e651804d8fc2efab18e76c
Alvaro <agarest1@alumno.upv.es>
2025-10-05 19:06:29 +0200

    Inversa Creada

--- DyV.h
+++ DyV.h
@@ -20,4 +20,23 @@ int BusquedaBinaria (const vector <T>& v, T x, int ini, int fin){
 		return BusquedaBinaria(v, x, medio + 1, fin);
 }
 
-#endif
+template <typename T>
+
+int BusquedaBinaria_INV (const vector <T>& v, T x, int ini, int fin){
+	
+	if (ini > fin)
+		return -1;
+	
+	int medio = (ini + fin) / 2;
+	
+	if (v[medio] == x)
+		return medio;
+	else if (v[medio] < x)
+		return BusquedaBinaria_INV(v, x, ini, medio -1);
+	else 
+		return BusquedaBinaria_INV(v, x, medio + 1 , fin);
+}
+
+#endif 
+
+
